c588cda80e5c808bf6c097c2728ee68e32b664e9
Matthew Jimenez <mattjimenez091@gmail.com>
2024-07-02 14:57:32 -0700

    add material ui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,13 @@ import { QueryClientProvider } from "@tanstack/react-query";
 
 import AppRouter from "./Router";
 import { queryClient } from "../lib/react-query";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <CssBaseline />
+
       <AppRouter />
     </QueryClientProvider>
   );
