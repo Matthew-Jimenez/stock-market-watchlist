9d18fdcacfab73c3cf30f3a1cda593ea4129a854
Matthew Jimenez <mattjimenez091@gmail.com>
2024-07-08 15:15:56 -0700

    add support for absolute imports

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -13,6 +14,14 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
+  },
+  webpackFinal: async (config) => {
+    config.resolve!.modules = [
+      ...(config.resolve!.modules || []),
+      path.resolve(__dirname, "../src"),
+    ];
+
+    return config;
   },
   staticDirs: ["../public"],
 
