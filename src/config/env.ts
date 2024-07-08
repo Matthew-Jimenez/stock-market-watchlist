a99194060625c95a0aae2065b9f864f97f9b7067
Matthew Jimenez <mattjimenez091@gmail.com>
2024-07-08 15:20:42 -0700

    correct env name

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ const createEnv = () => {
   return {
     BASE_API: process.env.REACT_APP_BASE_API || "",
     ALGOLIA_APP_ID: process.env.REACT_APP_ALGOLIA_APP_ID || "",
-    ALGOLIA_SEARCH_API_KEY: process.env.REACT_APP_ALGOLIA_SEARCH_API_KEY || "",
+    ALGOLIA_SEARCH_API_KEY: process.env.REACT_APP_ALGOLIA_SEARCH_KEY || "",
   };
 };
 
