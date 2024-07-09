427c68601cec970838e8d6f99ef4d025de29f33f
Matthew Jimenez <mattjimenez091@gmail.com>
2024-07-08 17:46:49 -0700

    use typography components

--- src/features/watchlist/components/list-item/component.tsx
+++ src/features/watchlist/components/list-item/component.tsx
@@ -1,5 +1,10 @@
-import { ListItemText } from "@mui/material";
+import { memo } from "react";
+
+import Typography from "components/typography";
+import Grid from "components/grid/component";
 import ListItemButton from "components/list/list-item-button/component";
+
+import toDollarValue from "utils/format/toDollarValue";
 
 interface Params {
   symbol?: string;
@@ -9,10 +14,16 @@ interface Params {
 const WatchlistListItem = ({ symbol, price }: Params) => {
   return (
     <ListItemButton>
-      <ListItemText primary={symbol} />
-      <ListItemText primary={price} />
+      <Grid container={true}>
+        <Grid item xs={4}>
+          <Typography variant="h6">{symbol}</Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant="body1">{toDollarValue(price)}</Typography>
+        </Grid>
+      </Grid>
     </ListItemButton>
   );
 };
 
-export default WatchlistListItem;
+export default memo(WatchlistListItem);
