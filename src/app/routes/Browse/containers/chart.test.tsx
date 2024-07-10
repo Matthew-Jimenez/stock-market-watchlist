0a2b13d8a68fa34464b36c7d7e6697252a8feddf
Matthew Jimenez <mattjimenez091@gmail.com>
2024-07-10 12:20:29 -0700

    refactor to remove duplicated logic

--- src/app/routes/Browse/containers/chart.test.tsx
+++ src/app/routes/Browse/containers/chart.test.tsx
@@ -1,54 +1,49 @@
 import { render, screen, waitFor } from "@testing-library/react";
+
+import mondayAtCloseData from "features/company-chart/stubs/intraday-history-one-day";
+import { setupMockServer } from "test-utils/mockServer";
+import QueryProvider from "test-utils/MockQueryProvider";
+
 import BrowseViewModel from "../model";
 import Chart from "./chart";
 import BrowseProvider from "../provider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import mondayAtCloseData from "features/company-chart/stubs/intraday-history-one-day";
-import { setupMockServer } from "test-utils/mockServer";
 
 jest.mock("../model");
 
 setupMockServer();
 
+const renderChartWithProvider = (model: BrowseViewModel) => {
+  render(
+    <BrowseProvider model={model}>
+      <Chart symbol="AAPL" width={100} height={100} />
+    </BrowseProvider>,
+    { wrapper: QueryProvider }
+  );
+};
+
 describe("Chart container", () => {
+  let model: BrowseViewModel;
+
+  beforeEach(() => {
+    model = new BrowseViewModel();
+    model.setChartComparePoint = jest.fn();
+  });
+
   describe("model.setChartComparePoint", () => {
     it("sets the compare point to undefined when the range is 1", () => {
-      const model = new BrowseViewModel();
-      model.setChartComparePoint = jest.fn();
+      renderChartWithProvider(model);
 
-      render(
-        <QueryClientProvider client={new QueryClient()}>
-          <BrowseProvider model={model}>
-            <Chart symbol="AAPL" width={100} height={100} />
-          </BrowseProvider>
-        </QueryClientProvider>
-      );
-
-      // Assert
       expect(model.setChartComparePoint).toHaveBeenCalledWith(undefined);
     });
 
     it("sets the compare point to the first data point when the range is not 1", async () => {
-      const model = new BrowseViewModel();
+      renderChartWithProvider(model);
 
-      model.setChartComparePoint = jest.fn();
+      screen.getByTestId(`button--1W`).click();
 
-      render(
-        <QueryClientProvider client={new QueryClient()}>
-          <BrowseProvider model={model}>
-            <Chart symbol="AAPL" width={100} height={100} />
-          </BrowseProvider>
-        </QueryClientProvider>
-      );
-
-      const range5Btn = screen.getByTestId(`button--1W`);
-
-      range5Btn.click();
-
+      const expectation = mondayAtCloseData[0];
       await waitFor(() => {
-        expect(model.setChartComparePoint).toHaveBeenCalledWith(
-          mondayAtCloseData[0]
-        );
+        expect(model.setChartComparePoint).toHaveBeenCalledWith(expectation);
       });
     });
   });
