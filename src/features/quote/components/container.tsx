3646ba171586afbb216015852db51466ad44e301
Matthew Jimenez <mattjimenez091@gmail.com>
2024-07-10 16:10:56 -0700

    use compare price override when hovering

--- src/features/quote/components/container.tsx
+++ src/features/quote/components/container.tsx
@@ -15,10 +15,15 @@ const QuoteContainer = ({
 }: Params) => {
   const { data } = useQuote({ symbol });
 
+  // If priceOverride is provided, prefer comparePriceOverride over data?.previousClose
+  // If priceOverride is not provided, use data?.previousClose as comparePrice
+  const comparePrice =
+    priceOverride !== undefined ? comparePriceOverride : data?.previousClose;
+
   return (
     <QuoteComponent
       price={priceOverride || data?.price}
-      comparePrice={comparePriceOverride || data?.previousClose}
+      comparePrice={comparePrice}
     />
   );
 };
